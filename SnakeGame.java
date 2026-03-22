60ab2fec59fa5b5c193c62e3de5604c90fb79e0c
Aparna Venkatesh <aparnavenkatesh2@gmail.com>
2026-03-21 22:05:28 -0400

    Add Snake Game files

---
+++ SnakeGame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+public class SnakeGame{
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Snake Game");
+        GamePanel panel= new GamePanel();
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+    }
+}
